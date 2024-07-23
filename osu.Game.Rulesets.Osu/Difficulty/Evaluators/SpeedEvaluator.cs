92b661345cc1f26d18eabedca94c430319ad681a
Givikap120 <givikap120@gmail.com>
2024-07-24 00:11:42 +0300

    some more cut-outs

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/SpeedEvaluator.cs
@@ -52,6 +52,10 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                 doubletapness = Math.Pow(speedRatio, 1 - windowRatio);
             }
 
+            // Cap deltatime to the OD 300 hitwindow.
+            // 0.93 is derived from making sure 260bpm OD8 streams aren't nerfed harshly, whilst 0.92 limits the effect of the cap.
+            strainTime /= Math.Clamp((strainTime / osuCurrObj.HitWindowGreat) / 0.93, 0.92, 1);
+
             return doubletapness * (1 + Math.Pow(75 / strainTime, 2.5));
         }
     }
