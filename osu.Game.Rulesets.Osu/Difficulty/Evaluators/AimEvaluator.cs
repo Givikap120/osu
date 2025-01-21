584744eb2c94b53dbc17d1b4e5ca80bc965d4f9a
Givikap120 <givikap120@gmail.com>
2025-01-21 17:12:27 +0200

    revert previous change as benifit is too small

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/AimEvaluator.cs
@@ -167,11 +167,10 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             double sliderJumpBonus = 1.0;
 
             // Punish the cases where 1/2 slider going into 1/2 note
-            if (osuCurrObj.BaseObject is not Slider)
-                sliderJumpBonus *= DifficultyCalculationUtils.ReverseLerp(osuLastObj.StrainTime, osuCurrObj.StrainTime * 0.55, osuCurrObj.StrainTime * 0.75);
+            sliderJumpBonus *= DifficultyCalculationUtils.ReverseLerp(osuLastObj.StrainTime, osuCurrObj.StrainTime * 0.55, osuCurrObj.StrainTime * 0.75);
 
             // Punish the cases where 1/2 slider going into two 1/2 notes
-            if (osuCurrObj.BaseObject is not Slider && sliderCurr == null)
+            if (sliderCurr == null)
                 sliderJumpBonus *= DifficultyCalculationUtils.ReverseLerp(osuCurrObj.StrainTime, osuLastObj.StrainTime * 0.55, osuLastObj.StrainTime * 0.75);
 
             // Punish too short sliders to prevent cheesing (cheesing is still possible, but it's very rare)
