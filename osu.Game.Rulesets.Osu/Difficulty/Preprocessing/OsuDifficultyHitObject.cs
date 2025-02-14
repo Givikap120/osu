fa11b7aacbacdd68ae3486dcfcea78f3f09adbd9
Givikap120 <givikap120@gmail.com>
2025-02-14 18:29:04 +0200

    fix compile errors for osu-tools

--- osu.Game.Rulesets.Osu/Difficulty/Preprocessing/OsuDifficultyHitObject.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Preprocessing/OsuDifficultyHitObject.cs
@@ -138,5 +138,11 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Preprocessing
 
             return pos;
         }
+
+        // To avoid compile errors with osu-tools
+        public double LazyJumpDistance => JumpDistance;
+        public double MinimumJumpDistance => JumpDistance;
+        public double MinimumJumpTime => DeltaTime;
+        public double TravelTime => 0;
     }
 }
