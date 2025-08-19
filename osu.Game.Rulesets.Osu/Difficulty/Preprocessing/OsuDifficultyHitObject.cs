acfd7cbeb643598599e325774818aa0bcd5225a3
Givikap120 <givikap120@gmail.com>
2025-08-20 00:07:49 +0300

    Don't use stacked position

--- osu.Game.Rulesets.Osu/Difficulty/Preprocessing/OsuDifficultyHitObject.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Preprocessing/OsuDifficultyHitObject.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Objects;
+using osu.Game.Rulesets.Objects.Types;
 using osu.Game.Rulesets.Osu.Objects;
 using osuTK;
 
@@ -42,7 +43,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Preprocessing
                 scalingFactor *= 1 + smallCircleBonus;
             }
 
-            Vector2 lastCursorPosition = LastObject.StackedPosition;
+            Vector2 lastCursorPosition = LastObject.Position;
             float lastTravelDistance = 0;
 
             if (LastObject is Slider lastSlider)
@@ -52,7 +53,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Preprocessing
                 lastTravelDistance = lastSlider.LazyTravelDistance;
             }
 
-            Distance = (lastTravelDistance + (BaseObject.StackedPosition - lastCursorPosition).Length) * scalingFactor;
+            Distance = (lastTravelDistance + (BaseObject.Position - lastCursorPosition).Length) * scalingFactor;
         }
 
         private void computeSliderCursorPosition(Slider slider)
@@ -66,7 +67,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Preprocessing
             foreach (var nestedHitObject in slider.NestedHitObjects)
             {
                 double progress = (nestedHitObject.StartTime - slider.StartTime) / slider.Duration;
-                var diff = slider.StackedPositionAt(progress) - slider.LazyEndPosition.Value;
+                var diff = slider.Position + slider.CurvePositionAt(progress) - slider.LazyEndPosition.Value;
                 float dist = diff.Length;
 
                 if (dist > approxFollowCircleRadius)
