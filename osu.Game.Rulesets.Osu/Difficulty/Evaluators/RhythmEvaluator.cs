318700b28a1bc055eb7b6b1cd3782d33368d2f8f
Givikap120 <givikap120@gmail.com>
2025-10-20 22:58:13 +0300

    Update RhythmEvaluator.cs

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/RhythmEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/RhythmEvaluator.cs
@@ -58,11 +58,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
 
                 double effectiveRatio = windowPenalty * currRatio;
 
-                if (double.IsNaN(effectiveRatio))
-                {
-                    Console.WriteLine("");
-                }
-
                 if (firstDeltaSwitch)
                 {
                     if (!(prevDelta > 1.25 * currDelta || prevDelta * 1.25 < currDelta))
