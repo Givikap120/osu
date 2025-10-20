6fa79c9711adf479501a64b08cd8c1de51b22e62
Givikap120 <givikap120@gmail.com>
2025-10-20 16:47:47 +0300

    Update OsuLegacyScoreMissCalculator.cs

--- osu.Game.Rulesets.Osu/Difficulty/OsuLegacyScoreMissCalculator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/OsuLegacyScoreMissCalculator.cs
@@ -27,7 +27,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             if (attributes.MaxCombo == 0 || score.LegacyTotalScore == null)
                 return 0;
 
-            double scoreV1Multiplier = 1; //attributes.LegacyScoreBaseMultiplier * getLegacyScoreMultiplier();
+            double scoreV1Multiplier = attributes.LegacyScoreBaseMultiplier * getLegacyScoreMultiplier();
             double relevantComboPerObject = calculateRelevantScoreComboPerObject();
 
             double maximumMissCount = calculateMaximumComboBasedMissCount();
@@ -74,7 +74,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty
             double objectsHit = (totalHits - countMiss) * combo / attributes.MaxCombo;
 
             // Score also has a non-combo portion we need to create the final score value.
-            double nonComboScore = 0;//(300 + attributes.SliderNestedScorePerObject) * score.Accuracy * objectsHit;
+            double nonComboScore = (300 + attributes.NestedScorePerObject) * score.Accuracy * objectsHit;
 
             return comboScore + nonComboScore;
         }
@@ -86,10 +86,10 @@ namespace osu.Game.Rulesets.Osu.Difficulty
         /// </summary>
         private double calculateRelevantScoreComboPerObject()
         {
-            double comboScore = 0; //attributes.MaximumLegacyComboScore;
+            double comboScore = attributes.MaximumLegacyComboScore;
 
             // We then reverse apply the ScoreV1 multipliers to get the raw value.
-            comboScore /= 300.0 / 25.0;// * attributes.LegacyScoreBaseMultiplier;
+            comboScore /= 300.0 / 25.0 * attributes.LegacyScoreBaseMultiplier;
 
             // Reverse the arithmetic progression to work out the amount of combo per object based on the score.
             double result = (attributes.MaxCombo - 2) * attributes.MaxCombo;
