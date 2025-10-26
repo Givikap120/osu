7e6f512e25ab5ac126015fbe2a0ed5c71969ceed
Givikap120 <givikap120@gmail.com>
2025-10-26 23:40:51 +0200

    Update OsuDifficultyHitObject.cs

--- osu.Game.Rulesets.Osu/Difficulty/Preprocessing/OsuDifficultyHitObject.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Preprocessing/OsuDifficultyHitObject.cs
@@ -88,6 +88,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Preprocessing
         public double MinimumJumpTime => DeltaTime;
         public double TravelTime => 0;
         public double TravelDistance => 0;
+        public Vector2? LazyEndPosition => null;
+        public double LazyTravelDistance => 0;
+        public double LazyTravelTime => 0;
         public double? Angle => null;
         public double GetDoubletapness(OsuDifficultyHitObject osuDifficultyHitObject) => 0;
     }
