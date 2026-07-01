121314b98987eec78d01ee875946b3290cf4821d
Givy120 <givikap120@gmail.com>
2026-07-01 07:39:45 +0200

    Merge pp-dev

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/AimEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/Aim/AimEvaluator.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
+using osu.Game.Rulesets.Difficulty.Utils;
 using osu.Game.Rulesets.Osu.Difficulty.Preprocessing;
 
 namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators.Aim
@@ -15,6 +15,6 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators.Aim
         /// <item><description>cursor velocity to the current object,</description></item>
         /// </list>
         /// </summary>
-        public static double EvaluateDifficultyOf(DifficultyHitObject current, bool _ = false) => Math.Pow(((OsuDifficultyHitObject)current).Distance, 0.99) / current.DeltaTime;
+        public static double EvaluateDifficultyOf(DifficultyHitObject current, bool _ = false) => DiffUtils.Pow(((OsuDifficultyHitObject)current).Distance, 0.99) / current.DeltaTime;
     }
 }
