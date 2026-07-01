1daf6bd419aa961d5e7f2b5b3f93b691d4b3cbea
Givy120 <givikap120@gmail.com>
2026-07-01 08:45:55 +0200

    Update Flashlight.cs

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Flashlight.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Flashlight.cs
@@ -1,14 +1,6 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
-// See the LICENCE file in the repository root for full licence text.
-
-using System;
-using System.Linq;
+﻿using System;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
-using osu.Game.Rulesets.Difficulty.Skills;
-using osu.Game.Rulesets.Difficulty.Utils;
 using osu.Game.Rulesets.Mods;
-using osu.Game.Rulesets.Osu.Difficulty.Evaluators;
-using osu.Game.Rulesets.Osu.Mods;
 
 namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 {
@@ -17,32 +9,18 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     /// </summary>
     public class Flashlight : OsuStrainSkill
     {
-        private readonly bool hasHiddenMod;
-
-        public Flashlight(Mod[] mods)
-            : base(mods)
+        public Flashlight(Mod[] mods) : base(mods)
         {
-            hasHiddenMod = mods.Any(m => m is OsuModHidden);
         }
 
-        private double skillMultiplier => 0.05512;
-        private double strainDecayBase => 0.15;
-        protected override double DecayWeight => 1.0;
-
-        private double currentStrain;
-
-        private double strainDecay(double ms) => DiffUtils.Pow(strainDecayBase, ms / 1000);
-
-        protected override double CalculateInitialStrain(double time, DifficultyHitObject current) => currentStrain * strainDecay(time - current.Previous(0).StartTime);
+        protected override double CalculateInitialStrain(double time, DifficultyHitObject current)
+        {
+            throw new NotImplementedException();
+        }
 
         protected override double StrainValueAt(DifficultyHitObject current)
         {
-            currentStrain *= strainDecay(current.DeltaTime);
-            currentStrain += FlashlightEvaluator.EvaluateDifficultyOf(current, hasHiddenMod) * skillMultiplier;
-
-            return currentStrain;
+            throw new NotImplementedException();
         }
-
-        public new static double DifficultyToPerformance(double difficulty) => 25 * Math.Pow(difficulty, 2);
     }
 }
